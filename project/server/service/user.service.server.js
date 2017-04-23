5f61b21c63f38a1426b10de377f9fbb08076c0bd
nancyhong8 <hong.na@husky.neu.edu>
2017-04-23 17:39:44 -0400

    m

--- project/server/service/user.service.server.js
+++ project/server/service/user.service.server.js
@@ -47,9 +47,9 @@ module.exports = function(app) {
     passport.deserializeUser(deserializeUser);
 
     var facebookConfig = {
-        clientID     : process.env.GOOGLE_CLIENT_ID,
+        clientID     : process.env.FACEBOOK_CLIENT_ID,
         //|| "283364938783133",
-        clientSecret : process.env.GOOGLE_CLIENT_SECRET,
+        clientSecret : process.env.FACEBOOK_CLIENT_SECRET,
         //|| "67049bb851f6a98b5da5385313932b24",
         callbackURL  : "http://localhost:3500/auth/facebook/callback"
     };
