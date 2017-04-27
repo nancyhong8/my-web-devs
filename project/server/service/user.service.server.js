d0782d23956068d83ad2d9ea85426b831a69663c
nancyhong8 <hong.na@husky.neu.edu>
2017-04-27 13:26:11 -0400

    m

--- project/server/service/user.service.server.js
+++ project/server/service/user.service.server.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
     app.get ('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
-            successRedirect: '/project/#!!/user/profile',
+            successRedirect: '/project/#!/user/profile',
             failureRedirect: '/project/#!/user/login'
         }));
 
